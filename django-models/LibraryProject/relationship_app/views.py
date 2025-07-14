1151f90b11dde829ee6f854c662819ac8d6e2b47
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-07-14 11:10:45 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django import forms
 
 # Custom form for Book operations
