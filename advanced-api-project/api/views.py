74290d2420683b9f0d5201402a4d54688945aa78
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-08-08 17:43:41 +0100

    task 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
