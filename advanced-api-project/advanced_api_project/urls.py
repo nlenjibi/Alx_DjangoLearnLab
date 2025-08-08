6195cbecdff57622dcb4c44797b45501ddee7aff
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-08-08 16:34:50 +0100

    first commit

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for LibraryProject project.
+URL configuration for advanced_api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-import debug_toolbar
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
