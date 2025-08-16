3e9d5a78b5633886920f8fdb5a2c36d5fbf9b60e
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-08-16 22:21:20 +0100

    update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'blog' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
