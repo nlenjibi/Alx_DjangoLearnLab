100ad0a1db10807b3a06cad3b69408eb05534766
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-08-17 00:35:38 +0100

    fix

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	bio = models.TextField(max_length=500, blank=True)
-	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+	profile_picture = models.ImageField(upload_to='media/', blank=True, null=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
