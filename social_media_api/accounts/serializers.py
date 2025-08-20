2ae8be47308433a5eddfb1d3d2c3e07f6cb9180d
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-08-20 16:05:54 +0100

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,8 +11,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user_model = get_user_model()
-        user = user_model.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
